58267a7c8da312c6d81a2f0f7f6f1efa230fcb5c
Roman <roman.kusmirchuk@gmail.com>
2021-12-11 18:12:34 +0200

    add laravel permision, admin controller

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    //
+//   Route::group(['middleware' => ['role:admin']], function () {
+//
+//   Route::get('/contact', function () {
+//       return view('contact');
+//   })->name('contact');
+//
+// });
+
+}
